f2eb5a345bb04e232b3390c17ae757a50ed58884
akless <andre.kless@web.de>
2026-09-26 00:15:06 +0200

    Update version in ccm.hello.mjs to 1.0.0

--- ccm.hello.mjs
+++ ccm.hello.mjs
@@ -7,7 +7,7 @@
  * @author André Kless <andre.kless@web.de>
  * @copyright 2025–2026 André Kless
  * @license MIT
- * @version 1.1.0-dev
+ * @version 1.0.0
  */
 export const component = {
   name: "hello",
